18392123ab33f490aa7de2a2e6d10df235a3a8b3
Abenzer Mulugeta <abenezermulugeta6020@gmail.com>
2021-01-14 13:23:25 +0300

    about us done!

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -72,3 +72,27 @@
   }, {
     offset: '80%'
   });
+
+
+  $('.owl-carousel').owlCarousel({
+  loop: true,
+  margin: 10,
+  nav: true,
+  navText: [
+    "<i class='fa fa-caret-left'></i>",
+    "<i class='fa fa-caret-right'></i>"
+  ],
+  autoplay: true,
+  autoplayHoverPause: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 3
+    },
+    1000: {
+      items: 3
+    }
+  }
+})
