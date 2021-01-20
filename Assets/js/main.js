30aa91a17121b36ed82bc894d4585e9d762c15fb
Abenzer Mulugeta <abenezermulugeta6020@gmail.com>
2021-01-20 11:11:12 +0300

    update

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -33,7 +33,20 @@
     $(".mobile-nav, .mobile-nav-toggle").hide();
   }
 
+   // Toggle .header-scrolled class to #header when page is scrolled
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+      $('#header').addClass('header-scrolled');
+    } else {
+      $('#header').removeClass('header-scrolled');
+    }
+  });
 
+  if ($(window).scrollTop() > 100) {
+    $('#header').addClass('header-scrolled');
+  }
+
+  
 
   $('.owl-carousel').owlCarousel({
   loop: true,
